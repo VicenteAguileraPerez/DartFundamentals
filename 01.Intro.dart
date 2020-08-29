4b7392399c58fc07cbfbe7a7613f393469b1a965
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-08-29 09:53:29 -0500

    Update 01.Intro.dart
    [Add comments]

--- 01.Intro.dart
+++ 01.Intro.dart
@@ -1,4 +1,7 @@
 //public static void main(String args[])
+//When you execute this proyect if you want to look at the if you need to put on the console next command
+// dart 01.Intro.dart cadena1 cadena2 cadena3
+// the args after the name of the code is the arguments of main method otherwise the result will be the else
 void main(List<String> args) {
   //dynamic var
   String nombre;
